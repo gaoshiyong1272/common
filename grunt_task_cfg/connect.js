005b260f68af74d7a8d092efefe7d699912e06e4
gaoshiyong1272 <gaoshiyong1272@vip.163.com>
2015-09-15 10:01:14 +0800

    #

---
+++ grunt_task_cfg/connect.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+/**
+ * 开启一个静态文件服务器
+ * */
+exports.options = {
+    port : 9800,
+    protocal : 'http',
+    hostname : 'localhost',
+    livereload : 79513
+};
+
+console.log('http://127.0.0.1:9801');
+
+/**
+ * 调试模式指向开发目录，部署模式指向上线目录
+ * */
+exports.server = {
+    options : {
+        open : true,
+        keepalive : true,
+        base : [
+            path.basename(gruntProject.debug ? gruntProject.prd : gruntProject.dest)
+        ]
+    }
+};
+
+console.log('connect config initialized');
