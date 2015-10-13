0f24be5fc48d3a49edf205b87780e374435ab646
gaoshiyong1272 <gaoshiyong1272@vip.163.com>
2015-10-13 18:14:34 +0800

    ！

--- source/common/scripts/constant/bar.js
+++
@@ -1,8 +0,0 @@
-(function(){
-	var bar = {
-		a:1,
-		b:2
-	}
-
-	module.exports = bar;
-})()
--- source/common/scripts/constant/foo.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	var bar = require('./bar.js');
-	function alerts(key){
-		console.log(bar[key]);
-	}
-	alerts('a');
-})();
